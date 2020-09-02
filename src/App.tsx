7b0c5eb0bafb929f266ae4a2480fba3c16470f42
Matheus Afonso <matheusafonsouza@gmail.com>
2020-09-02 18:58:19 -0300

    feature: Created login page front

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import SignIn from './pages/SignIn';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello!</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
