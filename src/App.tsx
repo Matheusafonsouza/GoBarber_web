55d781e39b80a01f33a8009d9726bbda76e5ee5c
Matheus Afonso <matheusafonsouza@gmail.com>
2020-09-07 16:48:36 -0300

    feature: Created Toast Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+import ToastContainer from './components/ToastContainer';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
