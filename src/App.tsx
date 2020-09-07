8c8ee5ff97522a27c2c123644a16d11deaebdfdd
Matheus Afonso <matheusafonsouza@gmail.com>
2020-09-07 17:07:52 -0300

    feature: Using context with toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
-import ToastContainer from './components/ToastContainer';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-
-    <ToastContainer />
+    </AppProvider>
 
     <GlobalStyle />
   </>
