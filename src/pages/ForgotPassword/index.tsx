dd1c8a4beb457dc2b35b22f86f869faeff93b199
matheusafonsouza <matheusafonsouza@gmail.com>
2020-10-28 15:05:58 -0300

    feat: created forgot password page

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,11 +1,9 @@
 import React, { useCallback, useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { FormHandles } from '@unform/core';
+import { FiLogIn, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
-
-import api from '../../services/api';
+import { FormHandles } from '@unform/core';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 import { useToast } from '../../hooks/toast';
@@ -17,44 +15,31 @@ import { Container, Content, AnimationContainer, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
-interface SignUpFormData {
-  name: string;
+interface ForgotPasswordFormData {
   email: string;
-  passworD: string;
 }
 
-const SignUp: React.FC = () => {
+const ForgotPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const history = useHistory();
 
   const { addToast } = useToast();
 
   const handleSubmit = useCallback(
-    async (data: SignUpFormData) => {
+    async (data: ForgotPasswordFormData) => {
       try {
         formRef.current?.setErrors({});
 
         const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatório'),
           email: Yup.string()
             .required('E-mail obrigatório')
             .email('Digite um E-mail válido'),
-          password: Yup.string().min(6, 'Senha deve ter no mínimo 6 digitos'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('/users', data);
-
-        addToast({
-          type: 'success',
-          title: 'Cadastro realizado!',
-          description: 'Você já pode fazer seu login no GoBarber!',
-        });
-
-        history.push('/');
+        // history.push('/dashboard');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -63,47 +48,39 @@ const SignUp: React.FC = () => {
         } else {
           addToast({
             type: 'error',
-            title: 'Erro no cadastro',
+            title: 'Erro na recuperação de senha',
             description:
-              'Ocorreu um erro ao tentar fazer cadastro, cheque seus dados',
+              'Ocorreu um erro ao tentar recuperar sua senha, cheque o email digitado',
           });
         }
       }
     },
-    [addToast, history],
+    [addToast],
   );
 
   return (
     <Container>
-      <Background />
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu cadastro</h1>
+            <h1>Recuperar senha</h1>
 
-            <Input icon={FiUser} name="name" placeholder="Nome" />
             <Input icon={FiMail} name="email" placeholder="E-mail" />
-            <Input
-              icon={FiLock}
-              name="password"
-              type="password"
-              placeholder="Senha"
-            />
-            <Button type="submit">Cadastrar</Button>
 
-            <a href="forgot">Esqueci minha senha</a>
+            <Button type="submit">Recuperar</Button>
           </Form>
 
           <Link to="/signin">
-            <FiArrowLeft />
-            Voltar para login
+            <FiLogIn />
+            Voltar ao login
           </Link>
         </AnimationContainer>
       </Content>
+      <Background />
     </Container>
   );
 };
 
-export default SignUp;
+export default ForgotPassword;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,15 @@ import { Switch } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route exact path="/" component={SignIn} />
+    <Route exact path="/signin" component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
