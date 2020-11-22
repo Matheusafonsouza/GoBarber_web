b226c2216b969e8a75c654c21017f338d75d9c38
Matheus Afonso <matheusafonsouza@gmail.com>
2020-11-22 14:08:11 -0300

    feat: created mock for signin test

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
-    Link: ({ children }: {children: React.ReactNode}) => children,
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
   }
 });
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
 
-    const { debug } = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+
+    fireEvent.change(emailField, { target: { value: 'matheusafonso@gmail.com' } });
+    fireEvent.change(passwordField, { target: { value: '123123123' } });
+
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   })
 })
